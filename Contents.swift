4e5f9d0f6dc1a2f5c7c4e29b45d1041985de831c
Alumno IDS <alumnoids@iMac-de-IT-Admin.local>
2019-02-21 18:24:50 -0600

    Primer Commit

--- Contents.swift
+++ Contents.swift
@@ -2,32 +2,13 @@
 
 import UIKit
 
-//Comparacion ASCII
-func compare(_ palabra1: String,_ palabra2: String) -> Bool {
-    if (UnicodeScalar(String(palabra1.prefix(1)))!.value > UnicodeScalar(String(palabra2.prefix(1)))!.value ) {
-        return true
-    }
-    return false
-}
-
-func findRotation(_ palabras: [String]) -> Int{
-    var index = 0
-    for i in stride(from: 0, to: palabras.count - 1, by: 1) {
-        if compare(palabras[i].lowercased(), palabras[i+1].lowercased()) {
-            index = i + 1
-            break
-        }
-    }
-    return index
-}
-
 var rotatedWords =  [
     "Pepe",
     "Retrógada",
     "Suplantar",
     "Uruguay",
     "Xonofobia",
-    // "zurco",
+    //"Zurco",
     "Asíntota",
     "Barba",
     "Baquero",
@@ -36,4 +17,29 @@ var rotatedWords =  [
     "Oracion"
 ]
 
-print(findRotation(rotatedWords))
+func Comparacion(_ palabra1: String,_ palabra2: String) -> Bool {
+    
+    if (UnicodeScalar(String(palabra1.prefix(1)))!.value > UnicodeScalar(String(palabra2.prefix(1)))!.value )
+    {
+        return true
+    }
+    
+    return false
+}
+
+func Rotacion(_ palabras: [String]) -> Int{
+    
+    var index = 0
+    
+    for i in stride(from: 0, to: palabras.count - 1, by: 1) {
+        
+        if Comparacion(palabras[i].lowercased(), palabras[i+1].lowercased())
+        {
+            index = i + 1
+            break
+        }
+    }
+    return index
+}
+
+print(Rotacion(rotatedWords))
